c380768f5db337df6b6f3b415ac16caa0d828744
Ronmy <ronmysalimey.itus@gmail.com>
2026-09-15 19:06:01 -0400

    chore: remove duplicate incomplete class 06

--- activities/class06/class-06-starter/src/server.js
+++
@@ -1,8 +0,0 @@
-// Entry point: this file only starts the process. It does not know about routes.
-import app from './app.js';
-
-const PORT = Number(process.env.PORT ?? 3000);
-
-app.listen(PORT, () => {
-  console.log(`Request API is running on http://localhost:${PORT}`);
-});
